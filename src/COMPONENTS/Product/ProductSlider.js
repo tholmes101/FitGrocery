e038ec07ba2d738775469bf32a13f60dcb9f8e06
Terence <piece4ulmind@gmail.com>
2024-10-29 10:47:06 -0400

    product details 1- 100% complete

--- src/COMPONENTS/Product/ProductSlider.js
+++ src/COMPONENTS/Product/ProductSlider.js
@@ -15,11 +15,11 @@ const ProductSlider = ({products, categoryname}) => {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 7
+      items: 4
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 4
+      items: 2
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
